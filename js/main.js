c2c671a47eb61172eaaa165e9a3839ad97798ba3
DrKillex <lcollaro1994@gmail.com>
2023-03-06 17:14:37 +0100

    completo

--- js/main.js
+++ js/main.js
@@ -32,7 +32,7 @@ function randomNumberArrayGenerator(howMany, between1, between2){    //funzione 
     return randomNumberArray
 };
 
-function numberInput (howMany){
+function numberInput (howMany){                                                 //funzione che registra e salva i numeri inserite dall utente
     while (chosenNumbers.length < howMany.length){
         let numberSelected = Number(prompt('inserisci un numero'))
         if (numberSelected !== NaN && numberSelected > 0){
@@ -42,7 +42,7 @@ function numberInput (howMany){
     return chosenNumbers  
 }
 
-function controlArrayContent (){
+function controlArrayContent (){                                            //funzione controllo contenuto array
     for (let i = 0; i < randomNumberArray.length; i++){
         if (randomNumberArray.includes(chosenNumbers[i])){
             rightNumbers.push(chosenNumbers[i])
@@ -50,23 +50,23 @@ function controlArrayContent (){
     }
 }
 
-function opacity(){
+function opacity(){                                         //funzione opacita
     div.classList.toggle('now-you-dont')
 }
 
 //main-----------------------------------------------------------------------------------------
 
-let randomNumberArray = [];
-let chosenNumbers = [];
-let rightNumbers = [];
-const div = document.getElementById('numeri')
-// howManyNumbers();
-randomNumberArrayGenerator(5, 1, 100);
-myAppend(5, div, 'box', randomNumberArray);
-setTimeout (opacity, 3000);
-setTimeout (function(){
+let randomNumberArray = [];      //array numeri random
+let chosenNumbers = [];             //array numeri scelti dall utente
+let rightNumbers = [];          //array numeri giusti
+const div = document.getElementById('numeri');          //seleziona dove mettere le scritte       
+
+randomNumberArrayGenerator(5, 1, 100);                  //genera 5 numeri random tra 1 e 100
+myAppend(5, div, 'box', randomNumberArray);             //appende i div contenenti i numeri casuali
+setTimeout (opacity, 28000);                            //timer dell opacita
+setTimeout (function(){                                 //timer della logica del gioco
     numberInput(randomNumberArray);
     controlArrayContent ();
     div.innerHTML = `hai ricordato esattamente questi numeri: ${rightNumbers} ossia ${rightNumbers.length} su 5`
     opacity();
-}, 10000)
+}, 31000)
